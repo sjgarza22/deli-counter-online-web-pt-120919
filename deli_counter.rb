23a0ec01d93d72e2dcfc40e644dfc0fc6aba059c
Sara Garza <sjgarza22@gmail.com>
2019-12-20 00:09:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+katz_deli = []
+def line(customers)
+  if customers.length > 0
+    string = "The line is currently:"
+    customers.each_with_index do |name, index|
+      string << " #{index + 1}. #{name}"
+    end
+    puts string
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
+
+
+
+katz_deli << "Ada"
+katz_deli << "Robby"
+line(katz_deli)
